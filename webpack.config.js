6731ca95a4e30e3ddaef80633135c6c4d9bb8710
JhordyMora <jhordy.mora@web.de>
2022-12-28 11:51:32 +0100

    htmlwebpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -19,5 +21,12 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./public/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
