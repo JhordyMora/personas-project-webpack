52be80a972338ee653be001fadd801e3c24533a2
JhordyMora <jhordy.mora@web.de>
2022-12-30 08:15:20 +0100

    fonts loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "main.js",
-        assetModuleFilename: "assets/images/[hash][ext][query]"
+        assetModuleFilename: "assets/images/[hash][ext]"
     },
     resolve: {
         extensions: [".js"]
